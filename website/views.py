8b775406dac5cb1b5b102473b899a4504238bea9
SleepyStew <sleepystew3@gmail.com>
2022-05-02 22:00:13 +1000

    robots

--- website/views.py
+++ website/views.py
@@ -1,10 +1,14 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, send_from_directory
 from flask_login import login_user, login_required, logout_user, current_user
 import flask_login
 from .models import User
 from .api import get_timetable
 
 views = Blueprint('views', __name__)  
+
+@views.route('/robots.txt')
+def robots():
+    return send_from_directory('static', 'robots.txt')
 
 @views.route('/')
 def home():
