9e017ed4a43a44b45cfd1d4f16e0ee563a6f123f
SleepyStew <75189901+SleepyStew@users.noreply.github.com>
2022-05-03 20:33:05 +1000

    fix

--- website/static/notes.js
+++ website/static/notes.js
@@ -1,5 +1,5 @@
 function deleteNote(noteId) {
-    fetch('/delete-note/', {
+    fetch('api/delete-note/', {
         method: 'POST',
         body: JSON.stringify({ note_id: noteId }),
     }).then((_res) => {
