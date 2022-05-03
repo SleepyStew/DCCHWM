036516375bf940e4a5fcd480c523a65a240a3c17
SleepyStew <75189901+SleepyStew@users.noreply.github.com>
2022-05-03 20:50:19 +1000

    fix

--- website/static/notes.js
+++ website/static/notes.js
@@ -3,6 +3,6 @@ function deleteNote(noteId) {
         method: 'POST',
         body: JSON.stringify({ note_id: noteId }),
     }).then((_res) => {
-        location.reload();
+        document.location = document.URL;
     });
 }
