5be93c6d9b8f6c53a9322456f25c9b4809d923b3
SleepyStew <75189901+SleepyStew@users.noreply.github.com>
2022-05-03 20:03:28 +1000

    update

--- website/models.py
+++ website/models.py
@@ -6,3 +6,10 @@ class User(db.Model, UserMixin):
     sbID = db.Column(db.Integer, unique=True)
     date = db.Column(db.DateTime, default=func.now())
     sbCookie = db.Column(db.String(64))
+    notes =  db.relationship('Note')
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer, db.ForeignKey('user.sbID'))
+    content = db.Column(db.String(512))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
