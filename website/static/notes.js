0313a8b8827ed7b9e9e6a7f9d20e10b875b197da
SleepyStew <75189901+SleepyStew@users.noreply.github.com>
2022-05-04 20:38:11 +1000

    fix

--- website/static/notes.js
+++ website/static/notes.js
@@ -57,5 +57,6 @@ function cancelEdit(noteId) {
     console.log(noteId);
     document.getElementById(noteId + "-edit-button").remove();
     document.getElementById(noteId + "-cancel-button").remove();
-    document.querySelector("#note-" + noteId).display = "visible";
+    document.getElementById(noteId + "edit").remove()
+    document.getElementById("note-" + noteId).style.display = "visible";
 }
