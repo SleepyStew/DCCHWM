2757cb5da24da3306938832d3a9296a395732bc3
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2022-05-04 19:37:22 +1000

    Update notes.js

--- website/static/notes.js
+++ website/static/notes.js
@@ -13,8 +13,20 @@ function editNote(noteId) {
     let editbox = document.createElement("textarea");
     editbox.classList.add("form-control");
     editbox.innerText = note.textContent.trim();
+    editbox.id = noteId + "-edit";
+
+    let savebutton = document.createElement("button");
+    savebutton.classList.add("btn");
+    savebutton.classList.add("btn-primary");
+    savebutton.innerText = "Save Note";
+    savebutton.onclick = "saveEdited(" + noteId + ")"
     
+    note.parentNode.insertBefore(savebutton, note.nextSibling);
     note.parentNode.insertBefore(editbox, note.nextSibling);
 
     note.style.display = "none";
+}
+
+function saveEdited(noteId) {
+    console.log(noteId);
 }
