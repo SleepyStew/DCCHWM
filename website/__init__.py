1e4de5611eda078be0cd1dae535df9cab223251e
SleepyStew <75189901+SleepyStew@users.noreply.github.com>
2022-05-06 06:27:32 +1000

    update

--- website/__init__.py
+++ website/__init__.py
@@ -30,9 +30,9 @@ def create_app():
     from .auth import auth
     from .api import api
 
-    admin = Admin(app, index_view=views.admin_index())
-    admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Note, db.session))
+    # admin = Admin(app, index_view=views.admin_index())
+    # admin.add_view(ModelView(User, db.session))
+    # admin.add_view(ModelView(Note, db.session))
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/auth/')
