042f3f8b7167f526243085a49def9cc4a3e1fa37
SleepyStew <75189901+SleepyStew@users.noreply.github.com>
2022-05-05 11:51:23 +1000

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ sys.dont_write_bytecode = True
 
 app = create_app()
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 if __name__ == '__main__':
     from waitress import serve
     serve(app, port=30015, host="0.0.0.0")
