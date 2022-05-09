6cd9f522675e0e32a504545b005b359c1759b587
SleepyStew <75189901+SleepyStew@users.noreply.github.com>
2022-05-09 16:18:05 +1000

    update

--- website/models.py
+++ website/models.py
@@ -7,7 +7,7 @@ class User(db.Model, UserMixin):
     date = db.Column(db.DateTime, default=func.now())
     sbCookie = db.Column(db.String(64))
     notes =  db.relationship('Note')
-    setting_alerts = db.Column(db.String(64))
+    setting_alerts = db.Column(db.String(64), default='high')
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
