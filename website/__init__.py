128bb781eafb46d151b5c880affa9d5d6d792c77
SleepyStew <75189901+SleepyStew@users.noreply.github.com>
2022-05-09 15:38:10 +1000

    update

--- website/__init__.py
+++ website/__init__.py
@@ -52,13 +52,9 @@ def create_app():
     create_database(app)
 
     return app
+    
 
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
         print("[?] Created database.")
-
-def add_column(engine, table_name, column):
-    column_name = column.compile(dialect=engine.dialect)
-    column_type = column.type.compile(engine.dialect)
-    engine.execute('ALTER TABLE %s ADD COLUMN %s %s' % (table_name, column_name, column_type))
