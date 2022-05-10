188d5b365d80fa7b9cb0b25de30ee7fef6d4e708
SleepyStew <75189901+SleepyStew@users.noreply.github.com>
2022-05-10 16:03:20 +1000

    update

--- website/__init__.py
+++ website/__init__.py
@@ -21,8 +21,8 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-    # csrf = CSRFProtect()
-    # csrf.init_app(app) # Compliant
+    csrf = CSRFProtect()
+    csrf.init_app(app) # Compliant
     print("[?] Setup config and initialised database.")
 
     login_manager = LoginManager()
