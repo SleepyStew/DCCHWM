83f4b20b1bddec9f84ab47a80f77faba98a17ff1
SleepyStew <75189901+SleepyStew@users.noreply.github.com>
2022-05-10 16:48:24 +1000

    bug fix

--- website/auth.py
+++ website/auth.py
@@ -36,16 +36,15 @@ def login():
             if login.status_code == 400 or login.status_code == 401:
                 flash("Incorrect username or password.", category='error')
             elif login.status_code == 200:
-                id = login.text.split('= {"id":')[1].split("\"")[0][:-1]
-                external_id = login.text.split('= {"id":')[1].split("\"")[3]
-                user = User.query.filter_by(sbID=id).first() is not None
+                sbid = login.text.split('= {"id":')[1].split("\"")[0][:-1]
+                user = User.query.filter_by(sbID=sbid).first() is not None
                 if user:
-                    User.query.filter_by(sbID=id).update(dict(id=str(login.cookies.get('PHPSESSID'))))
-                    User.query.filter_by(sbID=id).update(dict(sbCookie=str(login.cookies.get('PHPSESSID'))))
+                    User.query.filter_by(sbID=sbid).update(dict(id=str(login.cookies.get('PHPSESSID'))))
+                    User.query.filter_by(sbID=sbid).update(dict(sbCookie=str(login.cookies.get('PHPSESSID'))))
                     db.session.commit()
                     user_login = User.query.filter_by(sbID=id).first()
                 else:
-                    user_login = User(sbID=id, id = str(login.cookies.get('PHPSESSID')), sbCookie=str(login.cookies.get('PHPSESSID')))
+                    user_login = User(sbID=sbid, id = str(login.cookies.get('PHPSESSID')), sbCookie=str(login.cookies.get('PHPSESSID')))
                     db.session.add(user_login)
                     db.session.commit()
                     
