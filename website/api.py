65cf5bbe0d8081396ed536c2e7b857eae60afb92
SleepyStew <75189901+SleepyStew@users.noreply.github.com>
2022-05-11 23:08:46 +1000

    removed debug message

--- website/api.py
+++ website/api.py
@@ -103,7 +103,6 @@ def delete_note():
 def edit_note():
     note_id = json.loads(request.data)['note_id']
     note_content = json.loads(request.data)['note_content'].strip()
-    print(note_content)
     note = Note.query.get(note_id)
     if note and note.userID == current_user.sbID and note_is_valid(note_content):
         Note.query.filter_by(id=note_id).update(dict(content=note_content))
