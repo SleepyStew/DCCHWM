3dd676d1adbf172cfd8afa140d99816d62243db9
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2022-05-11 19:53:20 +1000

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = create_app()
 
 if __name__ == '__main__':
     from waitress import serve
+    register_template_filters(flask_app=app)
     print("[✔] Finished setting up webserver!")
     print("[?] Running webserver...")
     serve(app, port=30015, host="0.0.0.0")
