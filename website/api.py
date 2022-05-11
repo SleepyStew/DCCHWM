a1e4b3c94c342236b27907c8f4bd7426a2035502
SleepyStew <75189901+SleepyStew@users.noreply.github.com>
2022-05-11 21:28:10 +1000

    update

--- website/api.py
+++ website/api.py
@@ -9,6 +9,7 @@ import json
 from .models import Note, User
 from . import db
 import markdown
+import re
 
 api = Blueprint('api', __name__)  
 
@@ -78,6 +79,13 @@ def note_is_valid(note):
         return False
     return True
 
+def convert_to_markdown(note):
+    note = markdown.markdown(note)
+    soup = bs4.BeautifulSoup(note, 'html.parser')
+    for tag in soup.findAll(True):
+        tag.style = "display: inline;"
+    return note
+
 #########################################
 # Function above this | Endpoints below #
 #########################################
@@ -101,7 +109,7 @@ def delete_note():
 @login_required
 def edit_note():
     note_id = json.loads(request.data)['note_id']
-    note_content = markdown.markdown(json.loads(request.data)['note_content'].strip())
+    note_content = convert_to_markdown(json.loads(request.data)['note_content'].strip())
     note = Note.query.get(note_id)
     if note and note.userID == current_user.sbID and note_is_valid(note_content):
         Note.query.filter_by(id=note_id).update(dict(content=note_content))
@@ -115,7 +123,7 @@ def edit_note():
 @api.route('/create-note', methods=['POST'])
 @login_required
 def create_note():
-    note = markdown.markdown(request.form.get('note').strip())
+    note = convert_to_markdown(request.form.get('note').strip())
 
     if note_is_valid(note):
         new_note = Note(content=note, userID=current_user.sbID)
