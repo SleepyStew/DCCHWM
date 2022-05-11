507e40c5648a4a1b94522270d26499823d531216
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2022-05-11 19:44:50 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from website import create_app
+from flask import Flask
 import sys
 
 sys.dont_write_bytecode = True
@@ -10,3 +11,8 @@ if __name__ == '__main__':
     print("[✔] Finished setting up webserver!")
     print("[?] Running webserver...")
     serve(app, port=30015, host="0.0.0.0")
+    
+def register_template_filters(flask_app: Flask) -> None:
+    from website.filters import custom_template_filters
+    flask_app.register_blueprint(custom_template_filters.blueprint)
+    return None
