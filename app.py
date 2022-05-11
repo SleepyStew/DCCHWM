047738c56128408594f65e731513dbaa115c1647
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2022-05-11 20:01:58 +1000

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,8 @@ sys.dont_write_bytecode = True
 
 app = create_app()
 
-def register_template_filters(flask_app: Flask) -> None:
-    from website.filters import custom_template_filters
-    flask_app.register_blueprint(custom_template_filters.blueprint)
-    return None
-
 if __name__ == '__main__':
     from waitress import serve
-    register_template_filters(flask_app=app)
     print("[✔] Finished setting up webserver!")
     print("[?] Running webserver...")
     serve(app, port=30015, host="0.0.0.0")
