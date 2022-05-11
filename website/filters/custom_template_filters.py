f28eff3badf5ecc3f3b8b1ec19d31515d64dc387
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2022-05-11 19:48:33 +1000

    Update custom_template_filters.py

--- website/filters/custom_template_filters.py
+++ website/filters/custom_template_filters.py
@@ -4,12 +4,8 @@ from markupsafe import Markup, escape
 
 @pass_eval_context
 def newline_to_br(eval_ctx, value):
-    br = "<br>\n"
-
-    if eval_ctx.autoescape:
-        value = escape(value)
-        br = Markup(br)
-
+    value = escape(value)
+    br = Markup(br)
     result = "\n\n".join(
         f"<p>{br.join(p.splitlines())}<\p>"
         for p in re.split(r"(?:\r\n|\r(?!\n)|\n){2,}", value)
