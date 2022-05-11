3a82f68a9894cd3e492102ebc954031c2a99228f
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2022-05-11 19:51:31 +1000

    Update custom_template_filters.py

--- website/filters/custom_template_filters.py
+++ website/filters/custom_template_filters.py
@@ -1,13 +1,12 @@
-import re
-from jinja2 import pass_eval_context
-from markupsafe import Markup, escape
+from flask import Blueprint
+from jinja2 import evalcontextfilter, Markup, escape
 
-@pass_eval_context
-def newline_to_br(eval_ctx, value):
-    value = escape(value)
-    br = Markup(br)
-    result = "\n\n".join(
-        f"<p>{br.join(p.splitlines())}<\p>"
-        for p in re.split(r"(?:\r\n|\r(?!\n)|\n){2,}", value)
-    )
-    return Markup(result) if autoescape else result
+blueprint = Blueprint('custom_template_filters', __name__)
+
+@evalcontextfilter
+@blueprint.app_template_filter()
+def newline_to_br(context, value: str) -> str:
+    result = "<br />".join(re.split(r'(?:\r\n|\r|\n){2,}', escape(value)))
+    result = Markup(result)
+
+    return result
