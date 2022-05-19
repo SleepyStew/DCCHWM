825662cbb6a6b72b4f9ca5b0499775c85c3f559c
SleepyStew <75189901+SleepyStew@users.noreply.github.com>
2022-05-20 07:52:44 +1000

    update

--- website/api.py
+++ website/api.py
@@ -90,6 +90,11 @@ def get_upcoming_due_work(current_user):
         tag.find("div").find_all()[0].find("a")['style'] = "text-decoration: none;"
         tag.find("div").find_all()[0].find("a")['target'] = "_blank"
 
+        for subject, subject_value in friendly_subject_names.items():
+            if subject in tag.find("div").find_all()[0].text:
+                tag.find("div").find_all()[0].string.replace_with(subject_value + " - " + tag.find("div").find_all()[0].text)
+                break
+
         if "homework" in tag.find("div").find_all()[2].text.lower():
             tag.find("div").find_all()[2].clear()
             tag.find("div").find_all()[2].append("Homework")
