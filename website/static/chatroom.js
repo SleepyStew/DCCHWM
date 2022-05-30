ad780f65f7fd9e4506f27dc60e2160379faaec78
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2022-05-30 19:55:57 +1000

    Update chatroom.js

--- website/static/chatroom.js
+++ website/static/chatroom.js
@@ -14,10 +14,16 @@ document.getElementById('sendmessage').addEventListener('click', function() {
   document.getElementById('messageinput').value = '';
 });
 
+var prev = "";
+
 socket.on('chatmessage', function(data) {
-  // this was autofil don't judge my code | Nathan fix this
-  document.getElementById('messages').innerHTML += '<div class="message"><span class="username">' + data.username + '</span>: ' + data.message + '</div>';
+  if (prev == data.username) {
+    document.getElementById('messages').innerHTML += '<div class="message join"><span class="username">' + data.username + '</span>: ' + data.message + '</div>';
+  } else {
+    document.getElementById('messages').innerHTML += '<div class="message"><span class="username">' + data.username + '</span>: ' + data.message + '</div>';
+  }
   document.getElementById('messages').scrollTop = document.getElementById('messages').scrollHeight;
+  prev = data.username;
 });
 
 document.getElementById('messages').scrollTop = document.getElementById('messages').scrollHeight;
