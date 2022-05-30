3d0ded7d6bff9f776aa10a9ffcdc41ebab0398bc
SleepyStew <sleepystew3@gmail.com>
2022-05-30 17:56:28 +1000

    added basic chatroom

--- website/static/chatroom.js
+++ website/static/chatroom.js
@@ -13,3 +13,8 @@ document.getElementById('sendmessage').addEventListener('click', function() {
   socket.emit('chatmessage', { message: document.getElementById('messageinput').value });
   document.getElementById('messageinput').value = '';
 });
+
+socket.on('chatmessage', function(data) {
+  // this was autofil don't judge my code | Nathan fix this
+  document.getElementById('messages').innerHTML += '<div class="message"><span class="username">' + data.username + '</span>: ' + data.message + '</div>';
+});
