1fdc7b322af22b632e989ff7299175f56eac5922
SleepyStew <sleepystew3@gmail.com>
2022-05-31 16:36:19 +1000

    update

--- website/views.py
+++ website/views.py
@@ -46,7 +46,8 @@ def dashboard():
         logout_current_user()
         return redirect(url_for('auth.login'))
     timetable_headers = ["<div class=\"timetable-top\">Homegroup<br>8:40am-8:55am</div>", "<div class=\"timetable-top\">Period 1<br>9:00am-10:10am</div>", "<div class=\"timetable-top\">Period 2<br>10:30am-11:40am</div>", "<div class=\"timetable-top\">Period 3<br>11:45am-12:55pm</div>", "<div class=\"timetable-top\">Period 4<br>1:50pm-3:05pm</div>"]
-    print(zip(timetable, timetable_headers)[0])
+    print(zip(timetable, timetable_headers).nameList())
+    print(zip(timetable, timetable_headers).infoList())
     return render_template("dashboard.html", user=current_user, timetable=zip(timetable, timetable_headers), duework=duework)
 
 @views.route('/information')
