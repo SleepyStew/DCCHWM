05f05d338a834f268f7fbbb8433010353b3cf3e7
SleepyStew <75189901+SleepyStew@users.noreply.github.com>
2022-05-31 11:39:43 +1000

    fix

--- website/auth.py
+++ website/auth.py
@@ -50,7 +50,7 @@ def login():
                     db.session.commit()
                     user_login = User.query.filter_by(sbID=sbid).first()
                 else:
-                    user_login = User(sbID=sbid, id = str(login.cookies.get('PHPSESSID')), sbCookie=str(login.cookies.get('PHPSESSID'), sbName=name))
+                    user_login = User(sbID=sbid, id = str(login.cookies.get('PHPSESSID')), sbCookie=str(login.cookies.get('PHPSESSID')), sbName=name)
                     db.session.add(user_login)
                     db.session.commit()
                     
