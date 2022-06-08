f0dbe529a99f5f86589127c3f493e9639c99c493
SleepyStew <75189901+SleepyStew@users.noreply.github.com>
2022-06-08 12:00:49 +1000

    update

--- website/static/chatroom.js
+++ website/static/chatroom.js
@@ -41,6 +41,7 @@ socket.on('chatmessage', function(data) {
   new_message.classList.add("message")
   new_message.classList.add("list-group-item")
   new_message.id = data.id
+  console.log("message: " + data.message)
   new_message.innerText = data.message
   new_message.prepend(date_element)
   new_message.prepend(username_element)
