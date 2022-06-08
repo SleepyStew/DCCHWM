ca26180f37bb8979814a61e6925706759cac8b0a
SleepyStew <75189901+SleepyStew@users.noreply.github.com>
2022-06-08 11:23:56 +1000

    update

--- website/static/chatroom.js
+++ website/static/chatroom.js
@@ -25,6 +25,9 @@ document.getElementById('sendmessage').addEventListener('click', function() {
 });
 
 socket.on('chatmessage', function(data) {
+  let converter = new showdown.Converter();
+  converter.setOption('simpleLineBreaks', true)
+
   let username_element = document.createElement('span')
   username_element.innerText = data.username
   username_element.classList.add("username")
@@ -40,16 +43,13 @@ socket.on('chatmessage', function(data) {
   new_message.classList.add("list-group-item")
   new_message.id = data.id
   new_message.innerText = data.message
+  new_message.innerHTML = converter.makeHtml(new_message.innerHTML)
+  new_message.lastChild.style.display = 'inline';
   new_message.prepend(date_element)
   new_message.prepend(username_element)
   document.getElementById('messages').insertBefore(new_message, new_message.nextSibling)
   document.getElementById('messages').scrollTop = document.getElementById('messages').scrollHeight;
 
-  let converter = new showdown.Converter();
-  converter.setOption('simpleLineBreaks', true)
-  let messages = document.getElementsByClassName("message")
-  messages[messages.length - 1].innerHTML = converter.makeHtml(messages[messages.length - 1].innerHTML);
-  messages[messages.length - 1].lastChild.style.display = 'inline';
   if (document.getElementById("my-username").innerText == document.getElementById(data.id).getElementsByClassName("username")[0].innerText) {
     messages[messages.length - 1].classList.add("mine")
     messages[messages.length - 1].children[0].innerHTML += "<button type=\"button\" class=\"btn-close position-absolute end-0 me-2 delete-message\" alt=\"Delete Note\"></button>"
