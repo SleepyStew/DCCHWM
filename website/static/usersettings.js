7f942644ccd3c40fdaa7851847ebab32da1b56dc
SleepyStew <sleepystew3@gmail.com>
2022-06-09 19:14:58 +1000

    update

--- website/static/usersettings.js
+++ website/static/usersettings.js
@@ -1,10 +1,14 @@
 const editor = monaco.editor.create(document.getElementById('custom-js'), {
     value: document.getElementById('custom-javascript-default').innerText,
 	language: 'javascript',
-    theme: 'vs-dark'
+    theme: 'vs-dark',
+    automaticLayout: true,
+    minimap: {
+        enabled: false
+    }
 });
 
 function submit_custom_js() {
-    document.getElementById('custom-js-field').value = document.getElementsByClassName("view-lines")[0].innerText;
+    document.getElementById('custom-js-field').value = document.getElementsByClassName("view-lines")[0].innerText.replace(/\s/g, ' ');
     document.getElementById('custom-js-form').submit();
 }
