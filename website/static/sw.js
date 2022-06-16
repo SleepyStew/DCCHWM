d91a93f6667cbc10a93aefb1aad527f977cb023f
SleepyStew <sleepystew3@gmail.com>
2022-06-16 20:41:58 +1000

    update

--- website/static/sw.js
+++ website/static/sw.js
@@ -10,19 +10,6 @@ if (workbox) {
   );
 
   workbox.routing.registerRoute(
-    /\.(?:png|gif|jpg|jpeg|svg)$/,
-    workbox.strategies.cacheFirst({
-      cacheName: 'images',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 60,
-          maxAgeSeconds: 24 * 60 * 60, // 30 Days
-        }),
-      ],
-    }),
-  );
-
-  workbox.routing.registerRoute(
     new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
     workbox.strategies.cacheFirst({
       cacheName: 'googleapis',
