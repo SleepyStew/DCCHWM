2bd5a5d7cf98b3d6f87a24bd58fbb71b60b5e2fa
SleepyStew <sleepystew3@gmail.com>
2022-06-20 22:05:50 +1000

    update

--- website/static/pushnotifs.js
+++ website/static/pushnotifs.js
@@ -9,22 +9,21 @@ socket.on("messageAlert", function(data) {
 
     if (!Notification) {
         alert('Desktop notifications not available in your browser.');
-        return;
+    } else {
+        if (Notification.permission !== 'granted')
+        Notification.requestPermission();
+        
+        if (Notification.permission !== 'granted')
+        Notification.requestPermission();
+        else {
+        var notification = new Notification('You\'ve been mentioned.', {
+            icon: document.location.origin + '/static/pngs/logo.png',
+            body: 'Hey! ' + data.mentioner + ' mentioned you in Discussion.',
+        });
+        notification.onclick = function() {
+            window.open('http://stackoverflow.com/a/13328397/1269037');
+        }};
     }
-    
-    if (Notification.permission !== 'granted')
-    Notification.requestPermission();
-    
-    if (Notification.permission !== 'granted')
-    Notification.requestPermission();
-    else {
-    var notification = new Notification('You\'ve been mentioned.', {
-        icon: document.location.origin + '/static/pngs/logo.png',
-        body: 'Hey! ' + data.mentioner + ' mentioned you in Discussion.',
-    });
-    notification.onclick = function() {
-        window.open('http://stackoverflow.com/a/13328397/1269037');
-    }};
 });
 
 console.log("Loaded Push Notifications.");
